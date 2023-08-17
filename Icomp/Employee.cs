28ab032bf68c97626fd54ca73868ac8b584da0aa
girishjondhale <girishjondhale1997@gmail.com>
2023-08-16 21:36:48 -0700

    Add project files.

---
+++ Icomp/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Icopare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee { Salary = 10000, Name = "pankaj" };
+            Employee employee2 = new Employee { Salary = 20000, Name = "pratik" };
+
+            SalaryComparison sal = new SalaryComparison();
+            int result = sal.Compare(employee, employee2);
+
+            if (result < 0)
+            {
+                Console.WriteLine("Girish salary is less than sanket");
+            }
+            else if (result > 0)
+            {
+                Console.WriteLine("girish salary is more than sanket");
+            }
+            else
+            {
+                Console.WriteLine("salary of girish is same as sanket");
+            }
+
+
+        }
+    }
+}
